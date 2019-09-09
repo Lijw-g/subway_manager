5338ff5eaaf4baed0ec47b99aa9ca1bc1b3f2553
lijiwen <lijiwen@guazi.com>
2019-09-09 20:33:19 +0800

    changewer

---
+++ src/main/java/com/subway/manager/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.subway.manager.controller;
+
+/**
+ * @program: subway_manager
+ * @description: user info
+ * @author: lijiwen
+ * @create: 2019-09-09 20:32
+ **/
+public class UserController {
+}
